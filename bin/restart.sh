15a1eccdfea70c11ed7ddf12643f4df621d63574
Minchen Yu <myuaj@connect.ust.hk>
2019-01-17 21:15:50 +0800

    add restart script

---
+++ bin/restart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+echo "Copy conf dir"
+
+alluxio copyDir ../conf
+
+echo "Restart alluxio & hdfs"
+
+/home/ec2-user/alluxio/bin/alluxio-stop.sh all;
+/home/ec2-user/hadoop/sbin/stop-dfs.sh;
+/home/ec2-user/hadoop/sbin/start-dfs.sh;
+/home/ec2-user/alluxio/bin/alluxio format;
+/home/ec2-user/alluxio/bin/alluxio-start.sh all SudoMount
