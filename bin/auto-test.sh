23268d43d62d6e67173df6533b5c797bee52cf42
Minchen Yu <myuaj@connect.ust.hk>
2019-01-18 20:53:13 +0800

    update script

--- bin/auto-test.sh
+++ bin/auto-test.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-USER="$( cd "$( dirname "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" )"  && pwd )"
+DIR="$( cd "$( dirname "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" )"  && pwd )"
+
+echo "dir : $DIR"
 
 pre_data(){
     SCL=$1
-    cd $USER/tpch-spark/dbgen;
+    cd $DIR/tpch-spark/dbgen;
     ./dbgen -s $SCL;
 
-    cd $USER;
+    cd $DIR;
     rm -r data/;
     alluxio/bin/alluxio fs rm -R /tpch;
     mkdir data;
@@ -17,20 +19,20 @@ pre_data(){
 
 shuffle() {
     SCALE=$1
-    mkdir -p  $USER/logs/shuffle
+    mkdir -p  $DIR/logs/shuffle
 
     pre_data $SCALE
-    $USER/alluxio/bin/alluxio fs copyFromLocal $USER/data/lineitem.tbl /tpch/lineitem.tbl;
+    $DIR/alluxio/bin/alluxio fs copyFromLocal $DIR/data/lineitem.tbl /tpch/lineitem.tbl;
 
-    if [[ `cat ${USER}/alluxio/conf/threshold` == "1" ]]; then
-        echo "0" > ${USER}/alluxio/conf/threshold
+    if [[ `cat ${DIR}/alluxio/conf/threshold` == "1" ]]; then
+        echo "0" > ${DIR}/alluxio/conf/threshold
     else
-        echo "1" > ${USER}/alluxio/conf/threshold
+        echo "1" > ${DIR}/alluxio/conf/threshold
     fi
 
-    $USER/alluxio/bin/alluxio fs copyFromLocal $USER/data/orders.tbl /tpch/orders.tbl;
+    $DIR/alluxio/bin/alluxio fs copyFromLocal $DIR/data/orders.tbl /tpch/orders.tbl;
 
-    $USER/spark/bin/spark-submit --class "main.scala.TpchQuery" --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 $USER/tpch-spark/target/scala-2.11/spark-tpc-h-queries_2.11-1.0.jar 4 >  $USER/logs/shuffle/$SCALE.log
+    $DIR/spark/bin/spark-submit --class "main.scala.TpchQuery" --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 $DIR/tpch-spark/target/scala-2.11/spark-tpc-h-queries_2.11-1.0.jar 4 >  $DIR/logs/shuffle/$SCALE.log
 
 }
 
