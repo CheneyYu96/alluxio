e1796a469636884318c182371fe9febadba55407
Minchen Yu <myuaj@connect.ust.hk>
2019-02-22 22:25:04 +0800

    update concurrent test

--- bin/concurrent-test.sh
+++ bin/concurrent-test.sh
@@ -9,6 +9,7 @@ con_shuffle(){
     scale=$1
     query=$2
     concurrent=$3
+    core=$4
 
     sed -i "/alluxio.user.file.passive.cache.enabled=true/c\alluxio.user.file.passive.cache.enabled=false" \
     $DIR/alluxio/conf/alluxio-site.properties
@@ -19,7 +20,7 @@ con_shuffle(){
     mkdir -p $DIR/logs/shuffle
 
     for((c=1;c<=${concurrent};c++)); do
-        $DIR/spark/bin/spark-submit \
+        $DIR/spark/bin/spark-submit --executor-cores ${core} \
         --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 $DIR/tpch-spark/query/join.py \
         --query ${query} --app "shuffle: type${query} scale${scale} concurrency${c}" > $DIR/logs/shuffle/scale${scale}_con${c}.log 2>&1 &
     done
@@ -32,6 +33,7 @@ con_nonshuffle(){
     scale=$1
     query=$2
     concurrent=$3
+    core=$4
 
     sed -i "/alluxio.user.file.passive.cache.enabled=false/c\alluxio.user.file.passive.cache.enabled=true" \
     $DIR/alluxio/conf/alluxio-site.properties
@@ -52,7 +54,7 @@ con_nonshuffle(){
 
     # formal experiment
     for((c=1;c<=${concurrent};c++)); do
-        $DIR/spark/bin/spark-submit \
+        $DIR/spark/bin/spark-submit --executor-cores ${core} \
         --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 $DIR/tpch-spark/query/join.py \
         --query ${query} --app "noshuffle: type${query} scale${scale} concurrency${c}" > $DIR/logs/noshuffle/scale${scale}_con${c}.log 2>&1 &
     done
@@ -64,23 +66,24 @@ con_nonshuffle(){
 
 concurrent_test(){
     con_num=$1
+    core_num=$2
     upper_dir=/home/ec2-user/logs
     for((scl=12;scl<=18;scl=scl+6)); do #scale
         gen_data $scl
 
         for((j=0;j<=1;j++)); do #query
             query=$j
-            lower_dir=${upper_dir}/type${query}_scale${scl}_con${con_num}
+            lower_dir=${upper_dir}/type${query}_scale${scl}_con${con_num}_core${core_num}
             mkdir -p ${lower_dir}
 
             test_bandwidth ${lower_dir}
 
-            con_shuffle ${scl} ${query} ${con_num}
+            con_shuffle ${scl} ${query} ${con_num} ${core_num}
             mv $DIR/logs/shuffle ${lower_dir}
 
             ${DIR}/alluxio/bin/alluxio fs rm -R /tpch
 
-            con_nonshuffle ${scl} ${query} ${con_num}
+            con_nonshuffle ${scl} ${query} ${con_num} ${core_num}
             mv $DIR/logs/noshuffle ${lower_dir}
         done
 
@@ -90,16 +93,16 @@ concurrent_test(){
 
 
 usage() {
-    echo "Usage: $0 test # concurrency"
+    echo "Usage: $0 test # concurrency # cores"
 }
 
 
-if [[ "$#" -lt 2 ]]; then
+if [[ "$#" -lt 3 ]]; then
     usage
     exit 1
 else
     case $1 in
-        test)                   concurrent_test $2
+        test)                   concurrent_test $2 $3
                                 ;;
         * )                     usage
     esac
