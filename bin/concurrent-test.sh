cf014d63991159e2c2b57e6c3ac0bf61a60278e3
Minchen Yu <myuaj@connect.ust.hk>
2019-02-26 13:08:16 +0800

    add concurrent debug

--- bin/concurrent-test.sh
+++ bin/concurrent-test.sh
@@ -90,6 +90,98 @@ concurrent_test(){
     done
 }
 
+con_shuffle_debug() {
+    scale=$1
+    query=$2
+    concurrent=$3
+    core=$4
+
+    total_cores=$[$core*2]
+
+    sed -i \
+    "/alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.RoundRobinPolicy/c\alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.TimerPolicy" \
+    $DIR/alluxio/conf/alluxio-site.properties
+
+    sed -i "/alluxio.user.file.replication.min=2/c\alluxio.user.file.replication.min=0" $DIR/alluxio/conf/alluxio-site.properties
+
+    ${DIR}/alluxio/bin/restart.sh
+    move_data
+    clear_workerloads
+
+    mkdir -p $DIR/logs/shuffle
+
+    for((c=1;c<=${concurrent};c++)); do
+        $DIR/spark/bin/spark-submit --total-executor-cores ${total_cores} --executor-cores ${core} \
+        --conf spark.driver.extraJavaOptions="-agentpath:/home/ec2-user/async-profiler/build/libasyncProfiler.so=start,threads,interval=99700000,raw,event=cpu,file=/home/ec2-user/shuffle-profile-${c}.traces" \
+        --conf spark.executor.extraJavaOptions="-agentpath:/home/ec2-user/async-profiler/build/libasyncProfiler.so=start,threads,interval=99700000,raw,event=cpu,file=/home/ec2-user/shuffle-profile-${c}.traces" \
+        --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 $DIR/tpch-spark/query/join.py \
+        --query ${query} --app "shuffle: type${query} scale${scale} concurrency${c}" > $DIR/logs/shuffle/scale${scale}_con${c}.log 2>&1 &
+    done
+    wait
+
+    collect_workerloads shuffle shuffle
+}
+
+con_noshuffle_debug() {
+    scale=$1
+    query=$2
+    concurrent=$3
+    core=$4
+
+    sed -i \
+    "/alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.TimerPolicy/c\alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.RoundRobinPolicy" \
+    $DIR/alluxio/conf/alluxio-site.properties
+    sed -i "/alluxio.user.file.replication.min=0/c\alluxio.user.file.replication.min=2" $DIR/alluxio/conf/alluxio-site.properties
+
+    ${DIR}/alluxio/bin/restart.sh
+    move_data
+    clear_workerloads
+
+    mkdir -p $DIR/logs/noshuffle
+
+    # formal experiment
+    for((c=1;c<=${concurrent};c++)); do
+        $DIR/spark/bin/spark-submit \
+            --total-executor-cores ${total_cores} \
+            --executor-cores ${core} \
+            --conf spark.driver.extraJavaOptions="-agentpath:/home/ec2-user/async-profiler/build/libasyncProfiler.so=start,threads,interval=99700000,raw,event=cpu,file=/home/ec2-user/noshuffle-profile-${c}.traces" \
+            --conf spark.executor.extraJavaOptions="-agentpath:/home/ec2-user/async-profiler/build/libasyncProfiler.so=start,threads,interval=99700000,raw,event=cpu,file=/home/ec2-user/noshuffle-profile-${c}.traces" \
+            --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 \
+                $DIR/tpch-spark/query/join.py \
+                    --query ${query} \
+                    --app "noshuffle: type${query} scale${scale} concurrency${c}" > $DIR/logs/noshuffle/scale${scale}_con${c}.log 2>&1 &
+    done
+    wait
+
+    collect_workerloads noshuffle noshuffle
+}
+
+concurrent_test_debug() {
+    con_num=$1
+    core_num=$2
+    upper_dir=/home/ec2-user/logs
+    for((scl=12;scl<=12;scl=scl+6)); do #scale
+        gen_data $scl
+
+        for((j=0;j<=1;j++)); do #query
+            query=$j
+            lower_dir=${upper_dir}/type${query}_scale${scl}_con${con_num}_core${core_num}
+            mkdir -p ${lower_dir}
+
+            test_bandwidth ${lower_dir}
+
+            con_shuffle_debug ${scl} ${query} ${con_num} ${core_num}
+            mv $DIR/logs/shuffle ${lower_dir}
+
+
+            con_nonshuffle_debug ${scl} ${query} ${con_num} ${core_num}
+            mv $DIR/logs/noshuffle ${lower_dir}
+        done
+
+        clean_data
+    done
+}
+
 auto_test(){
     for((con=1;con<=16;con=con*2)); do
         cores=$[16/$con]
@@ -126,6 +218,8 @@ if [[ "$#" -lt 3 ]]; then
 else
     case $1 in
         test)                   concurrent_test $2 $3
+                                ;;
+        debug-test)             concurrent_test_debug $2 $3
                                 ;;
         auto)                   auto_test
                                 ;;
