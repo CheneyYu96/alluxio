282bd1db47d75d86f1eec1f680e926111dfb24b2
Minchen Yu <myuaj@connect.ust.hk>
2019-03-03 13:50:59 +0800

    recovery

--- core/client/fs/src/main/java/alluxio/client/file/policy/TimerPolicy.java
+++ core/client/fs/src/main/java/alluxio/client/file/policy/TimerPolicy.java
@@ -4,12 +4,14 @@ import alluxio.client.block.BlockWorkerInfo;
 import alluxio.client.block.policy.BlockLocationPolicy;
 import alluxio.client.block.policy.options.GetWorkerOptions;
 import alluxio.wire.WorkerNetAddress;
+import com.google.common.collect.Lists;
 
 import javax.annotation.concurrent.NotThreadSafe;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 /**
  *
@@ -19,21 +21,22 @@ import java.nio.file.Path;
  */
 @NotThreadSafe
 public class TimerPolicy implements FileWriteLocationPolicy, BlockLocationPolicy {
-//    private List<BlockWorkerInfo> mWorkerInfoList;
-    private String mWorkerName;
+    private List<BlockWorkerInfo> mWorkerInfoList;
+    private int mIndex;
 
-//    private boolean mInitialized = false;
+    private boolean mInitialized = false;
 
     /**
      * Constructs a new {@link TimerPolicy}.
      */
     public TimerPolicy() {
 
-        Path path = FileSystems.getDefault().getPath("/home/ec2-user/hadoop/conf/slaves");
+        Path path = FileSystems.getDefault().getPath("/home/ec2-user/alluxio/conf/threshold");
         try {
-            mWorkerName = Files.readAllLines(path).get(0).trim();
+            String index = Files.readAllLines(path).get(0);
+            mIndex = Integer.parseInt(index);
 
-            System.out.println(mWorkerName);
+            System.out.println(index);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,23 +45,17 @@ public class TimerPolicy implements FileWriteLocationPolicy, BlockLocationPolicy
     @Override
     public WorkerNetAddress getWorkerForNextBlock(Iterable<BlockWorkerInfo> workerInfoList,
                                                   long blockSizeBytes) {
-        for (BlockWorkerInfo info : workerInfoList) {
-            if (info.getNetAddress().getHost().equals(mWorkerName)) {
-                return info.getNetAddress();
-            }
-        }
-        return null;
 
-//        if (!mInitialized) {
-//            mWorkerInfoList = Lists.newArrayList(workerInfoList);
-//            mInitialized = true;
-//        }
-//
-//        if (mIndex >= mWorkerInfoList.size()) {
-//            mIndex = 0;
-//        }
-//
-//        return mWorkerInfoList.get(mIndex).getNetAddress();
+        if (!mInitialized) {
+            mWorkerInfoList = Lists.newArrayList(workerInfoList);
+            mInitialized = true;
+        }
+
+        if (mIndex >= mWorkerInfoList.size()) {
+            mIndex = 0;
+        }
+
+        return mWorkerInfoList.get(mIndex).getNetAddress();
     }
 
     @Override
