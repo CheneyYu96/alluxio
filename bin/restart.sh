c384b4c07aecbac2eeb723f039506231451be720
Minchen Yu <myuaj@connect.ust.hk>
2019-03-07 16:24:21 +0800

    convert

--- bin/restart.sh
+++ bin/restart.sh
@@ -10,12 +10,6 @@ echo "Copy conf dir"
 ./alluxio copyDir ../conf
 ./alluxio copyDir ../../spark/conf
 
-workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
-
-ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "sed -i '/alluxio.worker.hostname=localhost/c\alluxio.worker.hostname=${workers[0]}' /home/ec2-user/alluxio/conf/alluxio-site.properties"
-ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no "sed -i '/alluxio.worker.hostname=localhost/c\alluxio.worker.hostname=${workers[1]}' /home/ec2-user/alluxio/conf/alluxio-site.properties"
-
-
 echo "Restart alluxio & hdfs"
 
 /home/ec2-user/alluxio/bin/alluxio-stop.sh all;
