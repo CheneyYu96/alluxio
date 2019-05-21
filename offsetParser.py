664738384f126d696b910db1073e41d1938df352
郑云川 <yczheng1997@gmail.com>
2019-05-21 21:46:00 +0800

    Add Send Parquet Offset & Length Info
        bin/send-parquet-info.sh: scripts for sending parquet info
        jars: jars needed to be put in spark/jars
                spark - 2.4.3

--- offsetParser.py
+++ offsetParser.py
@@ -3,6 +3,7 @@
 import re
 import os
 import sys
+from pathlib import Path
 
 file = sys.argv[1]
 
@@ -11,10 +12,15 @@ f = open(file, 'r')
 index = 0
 offset_list = []
 length_list = []
+flag = False
 for line in f:
     line_list = line.split(' ')
     line_list = [x for x in line_list if x]
-    if (re.match('page',line_list[0])):
+    if (re.match('column', line_list[0])):
+        flag = False
+    elif (re.match('offset', line_list[0])):
+        flag = True
+    if (flag and re.match('page',line_list[0])):
         index +=1
         if (index % 2==0):
             offset_list.append(line_list[1])
@@ -22,9 +28,11 @@ for line in f:
 
 f.close()
 
-dir_name = os.path.dirname(file)
-fo = open(dir_name + "/offset.txt", 'w')
+# dir_name = os.path.dirname(file)
+dir_name = Path(file).resolve().parents[0]
 
+# fo = open(dir_name + "/offset.txt", 'w')
+fo = open(dir_name / "offset.txt", 'w')
 for i in range(len(offset_list)):
     fo.write(offset_list[i]+','+length_list[i]+'\n')
 
