134a412cbf72052fe5a0d9370656a051e23822d9
郑云川 <yczheng1997@gmail.com>
2019-05-27 10:44:31 +0800

    add fr-test2

---
+++ bin/fr-test2.sh
@@ -0,0 +1,223 @@
+#!/usr/bin/env bash
+#set -euxo pipefail
+set -x
+
+LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
+
+source ${LOCAL_DIR}/utils2.sh
+
+USE_PARQUER=0
+FROM_HDFS=0
+
+check_parquet(){
+    if [[ "${USE_PARQUER}" -eq "1" ]]; then
+        echo --run-parquet
+    fi
+}
+
+check_from_hdfs(){
+    if [[ "${FROM_HDFS}" -eq "1" ]]; then
+        echo --from-hdfs
+    fi
+}
+
+convert_test(){
+    scl=$1
+    gen_data $scl
+
+    convert
+}
+
+convert(){
+    if [[ ! -f ${DATA_SCALE} ]]; then
+        touch ${DATA_SCALE}
+    fi
+
+    if [[ `cat ${DATA_SCALE}` == "$SCL" && -d $DIR/tpch_parquet ]]; then
+        echo "Parquet exist"
+    else
+        non_fr_env
+        move_data
+
+        $DIR/spark/bin/spark-submit \
+            --executor-memory 4g \
+            --driver-memory 4g \
+            --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 \
+            $DIR/tpch-spark/target/scala-2.11/spark-tpc-h-queries_2.11-1.0.jar \
+                --convert-table \
+                $(check_from_hdfs ${FROM_HDFS})
+
+#       test one row group per part file
+#        $DIR/spark/bin/spark-submit \
+#            --executor-memory 4g \
+#            --driver-memory 4g \
+#            --master local \
+#            $DIR/tpch-spark/target/scala-2.11/spark-tpc-h-queries_2.11-1.0.jar \
+#                --convert-table \
+#                $(check_from_hdfs ${FROM_HDFS})
+
+        save_par_data
+    fi
+}
+
+trace_test(){
+    scl=$1
+    query=$2
+
+    dir_name=$(get_dir_index scale${scl}_query${query}_trace)
+    mkdir -p ${dir_name}
+
+    gen_data $scl
+    convert
+
+    USE_PARQUER=1
+    NEED_PAR_INFO=1
+
+    from=$query
+    to=$query
+    if [[ "${query}" -eq "0" ]]; then
+        from=1
+        to=22
+    fi
+
+    mkdir -p  $DIR/logs/shuffle
+
+    if [[ `cat ${ALLUXIO_ENV}` == "1" ]]; then
+        echo 'Alluxio env already prepared'
+    else
+        fr_env
+        $DIR/alluxio/bin/alluxio logLevel --logName=alluxio.master.repl.ReplManager --target=master --level=DEBUG
+
+        $DIR/alluxio/bin/alluxio logLevel \
+        --logName=alluxio.client.block.stream.BlockInStream \
+        --target=master,workers \
+        --level=DEBUG
+
+        move_par_data
+        if [[ "${NEED_PAR_INFO}" -eq "1" ]]; then
+             send_par_info
+        fi
+
+        echo '1' > ${ALLUXIO_ENV}
+    fi
+
+            #--conf spark.executor.extraJavaOptions="-Dlog4j.configuration=file://$DIR/tpch-spark/log4j.properties" \
+            #--conf spark.driver.extraJavaOptions="-Dlog4j.configuration=file://$DIR/tpch-spark/log4j.properties" \
+             #--log-trace \
+
+    for((q=${from};q<=${to};q++)); do
+        $DIR/spark/bin/spark-submit \
+            --executor-memory 4g \
+            --driver-memory 4g \
+            --conf spark.executor.extraJavaOptions="-Dlog4j.configuration=file://$DIR/tpch-spark/log4j.properties" \
+            --conf spark.driver.extraJavaOptions="-Dlog4j.configuration=file://$DIR/tpch-spark/log4j.properties" \
+            --master spark://$(cat /home/ec2-user/hadoop/conf/masters):7077 $DIR/tpch-spark/target/scala-2.11/spark-tpc-h-queries_2.11-1.0.jar \
+                --query ${q} \
+                $(check_parquet) \
+                --app-name "TPCH shuffle: scale${scl} query${q}" \
+                > $DIR/logs/shuffle/scale${scl}_query${q}.log 2>&1
+
+        collect_workerloads shuffle query${q}
+
+        line=$(cat $DIR/logs/shuffle/scale${scl}_query${q}.log | grep 'Got application ID')
+        appid=${line##*ID: }
+        echo "App ID: ${appid}"
+
+        mkdir -p $DIR/logs/shuffle/query${q}
+        collect_worker_logs shuffle/query${q} ${appid}
+    done
+
+    mv $DIR/logs/shuffle ${dir_name}
+
+}
+
+trace_range_test(){
+    scl=$(cat $DATA_SCALE)
+    start=$1
+    end=$2
+
+    for((qry=$start;qry<=$end;qry++)); do
+        trace_test $scl $qry
+    done
+}
+
+all_test(){
+    scl=$1
+    times=$2
+
+    for((t=0;t<${times};t++)); do
+        trace_test $scl 0
+    done
+}
+
+usage() {
+    echo "Usage: $0 shffl|noshffl scale #query"
+}
+
+INFO_DIR=${DIR}/info
+
+send_par_info(){
+
+    mkdir -p $INFO_DIR
+
+     for f in $(ls $DIR/tpch_parquet); do
+         mkdir -p $INFO_DIR/$f
+
+        IDX=0
+        for sf in $(ls ${DIR}/tpch_parquet/${f}); do
+
+            if [[ "${sf}" != "_SUCCESS" ]]; then
+                 extract_par_info ${DIR}/tpch_parquet/${f}/${sf} ${INFO_DIR}/${f}/tmp_${IDX}.txt ${INFO_DIR}/${f}/${IDX}.txt
+                java -jar ${DIR}/alluxio/writeparquet/target/writeparquet-2.0.0-SNAPSHOT.jar ${DIR}/tpch_parquet/${f}/${sf} ${INFO_DIR}/${f}/${IDX}.txt
+                ((IDX=IDX+1))
+            fi
+
+        done
+    done
+
+}
+
+
+extract_par_info(){
+    PAR_FILE=$1
+    TMP_FILE=$2
+    INFO_FILE=$3
+    hadoop jar ${DIR}/parquet-mr/parquet-cli/target/parquet-cli-1.12.0-SNAPSHOT-runtime.jar \
+        org.apache.parquet.cli.Main column-index ${PAR_FILE} > ${TMP_FILE}
+
+    python ${DIR}/alluxio/offsetParser.py ${TMP_FILE} ${INFO_FILE}
+
+}
+
+clear(){
+    remove $DIR/logs
+    remove $DIR/alluxio_env
+    remove $DIR/alluxio/logs
+}
+
+complie_job(){
+    cd $DIR/tpch-spark
+    git pull
+    sbt assembly
+}
+
+if [[ "$#" -lt 3 ]]; then
+    usage
+    exit 1
+else
+    case $1 in
+        conv)                   convert_test $2
+                                ;;
+        trace)                  trace_test $2 $3
+                                ;;
+        trace-range)            trace_range_test $2 $3
+                                ;;
+        all)                    all_test $2 $3
+                                ;;
+        clear)                  clear
+                                ;;
+        comp)                   complie_job
+                                ;;
+        * )                     usage
+    esac
+fi
---
+++ bin/utils2.sh
@@ -0,0 +1,231 @@
+#!/usr/bin/env bash
+#set -euxo pipefail
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
+DIR="$( cd "$DIR/../.." && pwd )"
+
+echo "dir : $DIR"
+DATA_SCALE=$DIR/data_scale
+ALLUXIO_ENV=$DIR/alluxio_env
+
+gen_data(){
+    SCL=$1
+
+    if [[ ! -f ${DATA_SCALE} ]]; then
+        touch ${DATA_SCALE}
+    fi
+
+    if [[ ! -f ${ALLUXIO_ENV} ]]; then
+        touch ${ALLUXIO_ENV}
+    fi
+
+    if [[ `cat ${DATA_SCALE}` == "$SCL" && -d ${DIR}/data ]]; then
+        echo "Data exist"
+    else
+        clean_data
+        cd $DIR/tpch-spark/dbgen
+        ./dbgen -f -s ${SCL}
+
+        cd $DIR
+
+        mkdir -p data
+        mv tpch-spark/dbgen/*.tbl data/
+
+        echo "${SCL}" > ${DATA_SCALE}
+        echo '0' > ${ALLUXIO_ENV}
+    fi
+}
+
+move_data(){
+    $DIR/alluxio/bin/alluxio fs mkdir $DIR/data
+
+    for f in $(ls $DIR/data); do
+        $DIR/alluxio/bin/alluxio fs copyFromLocal $DIR/data/$f $DIR/data/$f
+    done
+
+#    $DIR/alluxio/bin/alluxio fs copyFromLocal $DIR/data/lineitem.tbl $DIR/data/lineitem.tbl
+#    $DIR/alluxio/bin/alluxio fs copyFromLocal $DIR/data/orders.tbl $DIR/data/orders.tbl
+}
+
+move_data_hdfs(){
+    $DIR/hadoop/bin/hadoop fs mkdir -p $DIR/data
+    $DIR/alluxio/bin/alluxio fs mkdir $DIR/data
+
+    for f in $(ls $DIR/data); do
+        $DIR/hadoop/bin/hadoop fs -copyFromLocal $DIR/data/$f $DIR/data/$f
+    done
+}
+
+save_par_data(){
+    mkdir -p $DIR/tpch_parquet
+    $DIR/alluxio/bin/alluxio fs copyToLocal $DIR/tpch_parquet $DIR/tpch_parquet
+}
+
+move_par_data(){
+    $DIR/alluxio/bin/alluxio fs mkdir $DIR/tpch_parquet
+
+    for f in $(ls $DIR/tpch_parquet); do
+        $DIR/alluxio/bin/alluxio fs mkdir $DIR/tpch_parquet/$f
+        for sf in $(ls $DIR/tpch_parquet/$f); do
+            $DIR/alluxio/bin/alluxio fs copyFromLocal $DIR/tpch_parquet/$f/$sf $DIR/tpch_parquet/$f/$sf
+        done
+    done
+}
+
+clean_data(){
+    cd $DIR
+    if [[ -d data ]]; then
+        rm -r data/
+    fi
+
+    if [[ -d tpch_parquet ]]; then
+        rm -r tpch_parquet/
+    fi
+}
+
+free_limit(){
+    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+    ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "sudo wondershaper -c -a eth0; echo test"
+    ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no "sudo wondershaper -c -a eth0; echo test"
+}
+
+limit_bandwidth(){
+    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+    limit=$1
+    ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "sudo wondershaper -c -a eth0; sudo wondershaper -a eth0 -d $limit -u $limit"
+    ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no "sudo wondershaper -c -a eth0; sudo wondershaper -a eth0 -d $limit -u $limit"
+}
+
+test_bandwidth() {
+    saved_dir=$1
+
+    workers=(`cat /home/ec2-user/hadoop/conf/slaves`);
+
+    echo "Setup iperf3"
+    ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "iperf3 -s > /dev/null 2>&1 &"
+    ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no "iperf3 -c ${workers[0]} -d" >> ${saved_dir}/bandwith.txt
+
+    echo "kill iperf3 server process"
+    ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "pkill iperf3"
+}
+
+collect_worker_logs(){
+    worker_log_dir=$1
+    appid=$2
+
+    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+
+    worker0_id=$(check_executor_id ${workers[0]} ${appid})
+    scp -o StrictHostKeyChecking=no ec2-user@${workers[0]}:/home/ec2-user/spark/work/${appid}/${worker0_id}/stderr /home/ec2-user/logs/${worker_log_dir}/${worker0_id}.log
+
+    worker1_id=$(check_executor_id ${workers[1]} ${appid})
+    scp -o StrictHostKeyChecking=no ec2-user@${workers[1]}:/home/ec2-user/spark/work/${appid}/${worker1_id}/stderr /home/ec2-user/logs/${worker_log_dir}/${worker1_id}.log
+}
+
+check_executor_id(){
+    address=$1
+    appid=$2
+
+    exe_id=$(ssh ec2-user@${address} -o StrictHostKeyChecking=no ls /home/ec2-user/spark/work/${appid})
+    echo ${exe_id}
+}
+
+collect_workerloads(){
+    worker_log_dir=$1
+    name=$2
+
+    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+
+    if ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no test -e /home/ec2-user/logs/workerLoads.txt; then
+        scp -o StrictHostKeyChecking=no ec2-user@${workers[0]}:/home/ec2-user/logs/workerLoads.txt /home/ec2-user/logs/${worker_log_dir}/workerLoads0_${name}.txt
+        ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "rm /home/ec2-user/logs/workerLoads.txt"
+    fi
+
+    if ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no test -e /home/ec2-user/logs/workerLoads.txt; then
+        scp -o StrictHostKeyChecking=no ec2-user@${workers[1]}:/home/ec2-user/logs/workerLoads.txt /home/ec2-user/logs/${worker_log_dir}/workerLoads1_${name}.txt
+        ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no "rm /home/ec2-user/logs/workerLoads.txt"
+    fi
+}
+
+clear_workerloads(){
+    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+
+    if ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no test -e /home/ec2-user/logs/workerLoads.txt; then
+        ssh ec2-user@${workers[0]} -o StrictHostKeyChecking=no "rm /home/ec2-user/logs/workerLoads.txt"
+    fi
+
+    if ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no test -e /home/ec2-user/logs/workerLoads.txt; then
+        ssh ec2-user@${workers[1]} -o StrictHostKeyChecking=no "rm /home/ec2-user/logs/workerLoads.txt"
+    fi
+}
+
+shuffle_env(){
+    sed -i \
+        "/alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.RoundRobinPolicy/c\alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.TimerPolicy" \
+        $DIR/alluxio/conf/alluxio-site.properties
+
+    sed -i "/alluxio.user.file.replication.min=2/c\alluxio.user.file.replication.min=0" $DIR/alluxio/conf/alluxio-site.properties
+    sed -i "/alluxio.user.file.passive.cache.enabled=true/c\alluxio.user.file.passive.cache.enabled=false" $DIR/alluxio/conf/alluxio-site.properties
+
+    ${DIR}/alluxio/bin/restart.sh
+}
+
+nonshuffle_env(){
+    sed -i \
+        "/alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.TimerPolicy/c\alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.RoundRobinPolicy" \
+        $DIR/alluxio/conf/alluxio-site.properties
+
+    sed -i "/alluxio.user.file.replication.min=0/c\alluxio.user.file.replication.min=2" $DIR/alluxio/conf/alluxio-site.properties
+    sed -i "/alluxio.user.file.passive.cache.enabled=true/c\alluxio.user.file.passive.cache.enabled=false" $DIR/alluxio/conf/alluxio-site.properties
+
+    ${DIR}/alluxio/bin/restart.sh
+}
+
+fr_env(){
+
+    sed -i "/fr.client.translation=false/c\fr.client.translation=true" $DIR/alluxio/conf/alluxio-site.properties
+
+    sed -i "/alluxio.user.file.passive.cache.enabled=true/c\alluxio.user.file.passive.cache.enabled=false" $DIR/alluxio/conf/alluxio-site.properties
+
+    sed -i "/alluxio.user.file.replication.min=2/c\alluxio.user.file.replication.min=0" $DIR/alluxio/conf/alluxio-site.properties
+
+#    sed -i \
+#        "/alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.TimerPolicy/c\alluxio.user.file.copyfromlocal.write.location.policy.class=alluxio.client.file.policy.RoundRobinPolicy" \
+#        $DIR/alluxio/conf/alluxio-site.properties
+
+
+     ${DIR}/alluxio/bin/restart.sh
+}
+
+non_fr_env(){
+
+    sed -i "/fr.client.translation=true/c\fr.client.translation=false" $DIR/alluxio/conf/alluxio-site.properties
+
+     ${DIR}/alluxio/bin/restart.sh
+}
+
+get_dir_index(){
+    NAME=$1
+
+    INDEX=1
+    while [[ -d $DIR/logs/${NAME}${INDEX} ]]
+    do
+        let INDEX++
+    done
+
+    echo $DIR/logs/${NAME}${INDEX}
+
+}
+
+remove(){
+    OBJ=$1
+
+    if [[ -d ${OBJ} ]]; then
+        rm -r ${OBJ}
+    fi
+
+    if [[ -f ${OBJ} ]]; then
+        rm ${OBJ}
+    fi
+}
