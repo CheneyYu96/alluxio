a07aaafa0132bcfc571d2e01b618e68eb17faf62
Minchen Yu <myuaj@connect.ust.hk>
2019-06-14 15:18:50 +0800

    update

--- bin/conf-spark.sh
+++ bin/conf-spark.sh
@@ -26,7 +26,7 @@ add_name(){
 
         name=(`echo ${workers[$((i*2+1))]} | cut -d '(' -f 2 | cut -d ')' -f 1`)
         echo ${name}
-        ssh ec2-user@${w_ip} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${name}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
+        ssh ec2-user@${w_ip} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${w_ip}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
     done
 }
 
