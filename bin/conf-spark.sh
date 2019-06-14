0d79957919397800de7cb4d55d76595cfe73f8f6
Minchen Yu <myuaj@connect.ust.hk>
2019-06-14 15:09:07 +0800

    update

--- bin/conf-spark.sh
+++ bin/conf-spark.sh
@@ -15,13 +15,13 @@ add_name(){
 #        ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${workers[$i]}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
 #    done
 
-    workers=(`hdfs dfsadmin -printTopology | grep 172`)
-    worker_num=(`hdfs dfsadmin -printTopology | grep 172 | wc -l`)
+    workers=(`hdfs dfsadmin -printTopology | grep ip`)
+    worker_num=(`hdfs dfsadmin -printTopology | grep ip | wc -l`)
     worker_num=$(($worker_num-1))
 
     for i in `seq 0 ${worker_num}`; do
         w_ip=(`echo ${workers[$i]} | cut -d ':' -f 1`)
-        w_ip=${w_ip:3}
+#        w_ip=${w_ip:3}
 
         echo ${w_ip}
 
