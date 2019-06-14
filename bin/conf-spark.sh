1e53f8eb78f538251c1644bdbcbda61fbad0e023
Minchen Yu <myuaj@connect.ust.hk>
2019-06-14 14:25:31 +0800

    conf spark

--- bin/conf-spark.sh
+++ bin/conf-spark.sh
@@ -11,5 +11,5 @@ worker_num=(`cat /home/ec2-user/hadoop/conf/slaves | wc -l`)
 worker_num=$(($worker_num-2))
 
 for i in `seq 0 ${worker_num}`; do
-    ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=${workers[$i]}' >>  /home/ec2-user/spark/conf/spark-env.sh"
+    ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${workers[$i]}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
 done
