2a9ca3f2647ee2b22081c74670a25bf4cd9da4bf
Minchen Yu <myuaj@connect.ust.hk>
2019-06-14 15:06:46 +0800

    update

--- bin/conf-spark.sh
+++ bin/conf-spark.sh
@@ -6,13 +6,28 @@ LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
 source ${LOCAL_DIR}/utils.sh
 
 add_name(){
-    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+#    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
+#
+#    worker_num=(`cat /home/ec2-user/hadoop/conf/slaves | wc -l`)
+#    worker_num=$(($worker_num-2))
+#
+#    for i in `seq 0 ${worker_num}`; do
+#        ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${workers[$i]}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
+#    done
 
-    worker_num=(`cat /home/ec2-user/hadoop/conf/slaves | wc -l`)
-    worker_num=$(($worker_num-2))
+    workers=(`hdfs dfsadmin -printTopology | grep 172`)
+    worker_num=(`hdfs dfsadmin -printTopology | grep 172 | wc -l`)
+    worker_num=$(($worker_num-1))
 
     for i in `seq 0 ${worker_num}`; do
-        ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${workers[$i]}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
+        w_ip=(`echo ${workers[$i]} | cut -d ':' -f 1`)
+        w_ip=${w_ip:3}
+
+        echo ${w_ip}
+
+        name=(`echo ${workers[$i]} | cut -d '(' -f 2 | cut -d ')' -f 1`)
+        echo ${name}
+#        ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "echo 'export SPARK_LOCAL_HOSTNAME=\"${workers[$i]}\"' >>  /home/ec2-user/spark/conf/spark-env.sh"
     done
 }
 
