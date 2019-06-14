307d34761ba441c2950f29328411c40621d3d04c
Minchen Yu <myuaj@connect.ust.hk>
2019-06-14 14:35:16 +0800

    update

--- bin/conf-spark.sh
+++ bin/conf-spark.sh
@@ -17,14 +17,8 @@ add_name(){
 }
 
 remove_last(){
-    workers=(`cat /home/ec2-user/hadoop/conf/slaves`)
-
-    worker_num=(`cat /home/ec2-user/hadoop/conf/slaves | wc -l`)
-    worker_num=$(($worker_num-2))
-
-    for i in `seq 0 ${worker_num}`; do
-        ssh ec2-user@${workers[$i]} -o StrictHostKeyChecking=no "sed -i \"\\$d\" /home/ec2-user/spark/conf/spark-env.sh"
-    done
+    cd ${LOCAL_DIR}
+    ./alluxio copyDir ../../spark/conf
 }
 
 usage(){
