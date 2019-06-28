7b498b397a9675e62ef7ee894e5c37a9a6c6b24c
Minchen Yu <myuaj@connect.ust.hk>
2019-06-28 15:07:21 +0800

    update test

--- bin/utils.sh
+++ bin/utils.sh
@@ -203,7 +203,8 @@ conv_env(){
     sed -i "/fr.client.translation=true/c\fr.client.translation=false" $DIR/alluxio/conf/alluxio-site.properties
     sed -i "/fr.client.block.location=true/c\fr.client.block.location=false" $DIR/alluxio/conf/alluxio-site.properties
 
-    sed -i "/alluxio.user.block.size.bytes.default=1GB/c\alluxio.user.block.size.bytes.default=900MB" $DIR/alluxio/conf/alluxio-site.properties
+    sed -i '/^alluxio.user.block.size.bytes.default=/calluxio.user.block.size.bytes.default=800MB' $DIR/alluxio/conf/alluxio-site.properties
+
      ${DIR}/alluxio/bin/restart.sh
 }
 
