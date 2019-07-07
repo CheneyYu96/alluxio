e9e7f38751004571579e51251e96cd3a1ba725c4
Minchen Yu <myuaj@connect.ust.hk>
2019-07-07 13:50:02 +0800

    update test

--- bin/io-test.sh
+++ bin/io-test.sh
@@ -197,7 +197,7 @@ complie_job(){
 #####################
 #  call python test script
 #####################
-limit=1000000
+limit=3000000
 DIST=0
 
 run_default(){
@@ -449,10 +449,10 @@ skew_band_test(){
 
 spec_test(){
     timeout=$1
-    PER_COL=3
+    PER_COL=1
 
 #    for bdgt in "0.5" "1" "2"; do
-    for bdgt in "2"; do
+    for bdgt in "0.5" "2"; do
         sed -i "/^fr.repl.budget=/cfr.repl.budget=${bdgt}" ${DIR}/alluxio/conf/alluxio-site.properties
 
         policy_env
@@ -469,8 +469,8 @@ spec_test(){
 
         sleep ${sleep_time} # wait util replication finished
 
-#        for rt in 20 30 40; do
-        for rt in 30; do
+        for rt in 20 30 40; do
+#        for rt in 30; do
 
             mkdir -p $DIR/logs/r${rt}_b${bdgt}
 
