0031515ac55a841e28aca2b9d89ec4652088a259
Minchen Yu <myuaj@connect.ust.hk>
2019-07-07 20:39:13 +0800

    update test

--- bin/io-test.sh
+++ bin/io-test.sh
@@ -456,7 +456,7 @@ spec_test(){
 #    rm_env_except_pattern
 
 #    for bdgt in "0.5" "1" "2"; do
-    for bdgt in "1"; do
+    for bdgt in "0.5" "2"; do
         sed -i "/^fr.repl.budget=/cfr.repl.budget=${bdgt}" ${DIR}/alluxio/conf/alluxio-site.properties
 
         policy_env
@@ -474,7 +474,7 @@ spec_test(){
         sleep ${sleep_time} # wait util replication finished
 
 #        for rt in 20 30 40; do
-        for rt in 40; do
+        for rt in 30 40; do
 
             mkdir -p $DIR/logs/r${rt}_b${bdgt}
 
