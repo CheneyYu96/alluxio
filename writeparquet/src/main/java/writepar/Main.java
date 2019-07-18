d8e543c41b8bbc4c88fb741838496f515705a914
Minchen Yu <myuaj@connect.ust.hk>
2019-07-18 16:18:12 +0800

    update write parquet

--- writeparquet/src/main/java/writepar/Main.java
+++ writeparquet/src/main/java/writepar/Main.java
@@ -8,14 +8,34 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1 && args.length % 2 == 0){
-            ParquetInfo parquetInfo = new ParquetInfo();
-            for (int i = 0; i < args.length; i=i+2){
-                parquetInfo.sendInfo(args[i], args[i+1]);
+        ParquetInfo parquetInfo = new ParquetInfo();
+        if (args.length > 0){
+            switch (args[0]){
+                case "test":
+                    parquetInfo.test(args[1]);
+                    break;
+
+                case "alpha":
+                    int fileNum = Integer.parseInt(args[1]);
+                    int offNum = Integer.parseInt(args[2]);
+                    parquetInfo.alphaTest(fileNum, offNum);
+                    break;
+
+                case "info":
+                    int isRecord = Integer.parseInt(args[1]);
+                    parquetInfo.sendInfo(args[2], args[3], isRecord);
+                    break;
+
+                case "write":
+                    int isThrt = Integer.parseInt(args[1]);
+                    int isRepl = Integer.parseInt(args[2]);
+
+                    parquetInfo.writeParquet(isThrt, isRepl, args[3]);
             }
         }
+
         else {
-            System.out.println("Require <file path, info path>");
+            System.out.println("Require <file path, info path> or <location path>");
         }
     }
 }
--- writeparquet/src/main/java/writepar/ParquetInfo.java
+++ writeparquet/src/main/java/writepar/ParquetInfo.java
@@ -1,17 +1,25 @@
 package writepar;
 
 import alluxio.AlluxioURI;
+import alluxio.client.WriteType;
 import alluxio.client.file.FileSystem;
+import alluxio.client.file.FileSystemContext;
+import alluxio.client.file.URIStatus;
+import alluxio.client.file.options.CreateFileOptions;
+import alluxio.client.file.policy.SpecificHostPolicy;
+import alluxio.exception.AlluxioException;
 import alluxio.util.CommonUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import alluxio.wire.FileBlockInfo;
+import alluxio.wire.WorkerNetAddress;
+import fr.client.file.FRFileWriter;
+import fr.client.utils.FRUtils;
+import fr.client.utils.OffLenPair;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -19,25 +27,25 @@ import java.util.List;
 public class ParquetInfo {
     private FileSystem mFileSystem;
 
-    private static final Logger LOG = LoggerFactory.getLogger(ParquetInfo.class);
-
+    private final String locationsFile;
 
     public ParquetInfo() {
         mFileSystem = FileSystem.Factory.get();
+        locationsFile = System.getProperty("user.dir") + "/origin-locs.txt";
+
+
     }
 
-    public void sendInfo(String filePath, String infoPath) throws IOException{
+    public void alphaTest(int fileNum, int offNum) throws IOException{
+        List<Long> lengthes = IntStream.range(0, offNum).mapToLong( i -> 100).boxed().collect(Collectors.toList());
+        List<Long> offsets = IntStream.range(0, offNum).mapToLong( i -> i * 100).boxed().collect(Collectors.toList());
+
+        String tag = "<alpha>" + fileNum;
+        mFileSystem.sendParquetInfo(new AlluxioURI(tag), offsets, lengthes);
+    }
+
+    public void sendInfo(String filePath, String infoPath, int isRecord) throws IOException{
         long startTimeMs = CommonUtils.getCurrentMs();
-
-        Runtime run = Runtime.getRuntime();
-
-//        String absPath = System.getProperty("user.dir");
-//        String outputPath = absPath + "/output.txt";
-//        String pythonPath = absPath + "/offsetParser.py";
-//        String infoPath = absPath + "/offset.txt";
-
-//        run.exec("parquet column-index " + localFilePath + " > " + outputPath);
-//        run.exec("python3 " + pythonPath + " " + outputPath);
 
         List<Long> offset = new ArrayList<>();
         List<Long> length = new ArrayList<>();
@@ -59,6 +67,111 @@ public class ParquetInfo {
 
         mFileSystem.sendParquetInfo(new AlluxioURI(filePath), offset, length);
 
-        LOG.info("Send parquet file info. " + filePath + "; elapsed:" + (CommonUtils.getCurrentMs() - startTimeMs));
+        System.out.println("Send parquet file info. " + filePath + "; elapsed:" + (CommonUtils.getCurrentMs() - startTimeMs));
+
+        if (isRecord == 1) {
+            recordLoc(filePath);
+        }
+
+    }
+
+    private void recordLoc(String filePath) throws IOException {
+        WorkerNetAddress address = FRUtils.getFileLocation(new AlluxioURI(filePath), mFileSystem, FileSystemContext.get());
+        if (address == null){
+            System.err.println("File block address is null");
+            return;
+        }
+        FileWriter fw = new FileWriter(locationsFile, true);
+        fw.write(filePath + "," + address.getHost() + "\n");
+        fw.close();
+        System.out.println("Write loc for file " + filePath);
+
+    }
+
+    public void test(String filePath){
+        try {
+            URIStatus orginStatus = mFileSystem.getStatus(new AlluxioURI(filePath));
+
+            List<FileBlockInfo> fileBlockInfos = orginStatus.getFileBlockInfos();
+
+            System.out.println("file block info sizes: " + fileBlockInfos.size());
+
+            System.out.println(fileBlockInfos);
+
+        } catch (IOException | AlluxioException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void writeParquet(int isThrt, int isRepl, String locationFile) throws IOException {
+
+        WriteType writeTpye = isThrt==1 ? WriteType.CACHE_THROUGH : WriteType.MUST_CACHE;
+
+        System.out.println("Throttle: " + isThrt + ". Write Type: " + writeTpye);
+
+        FileInputStream is = new FileInputStream(locationFile);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line;
+//        int idx = 0;
+        while ((line = reader.readLine()) != null) {
+            String[] splitLine = line.split(",");
+            String path = splitLine[0];
+            String address = splitLine[1];
+
+//            path = "/test/" + idx++;
+
+            FRFileWriter writer = new FRFileWriter(new AlluxioURI(path));
+            writer.setWriteOption(CreateFileOptions.defaults().setWriteType(writeTpye).setLocationPolicy(new SpecificHostPolicy(address)));
+
+            FileInputStream localFileStream;
+            byte[] tBuf;
+
+            if (isRepl == 1){
+                System.out.println("Write replica: " + path + " on address: " + address);
+
+                String sourceFile = splitLine[2];
+                List<OffLenPair> pairs = new ArrayList<>();
+                for (int i = 3; i < splitLine.length; i++){
+                    String[] pairSpl = splitLine[i].split(":");
+                    pairs.add(new OffLenPair(Long.parseLong(pairSpl[0]), Long.parseLong(pairSpl[1])));
+                }
+
+                localFileStream = new FileInputStream(sourceFile);
+
+                long length = pairs
+                        .stream()
+                        .mapToLong( o -> o.length)
+                        .sum();
+
+                System.out.println("Source: " + sourceFile + ". len: " + length);
+
+                tBuf = new byte[(int) length];
+                int tBytesRead = localFileStream.read(tBuf);
+
+            }
+            else {
+                System.out.println("Write source file: " + path + " on address: " + address);
+
+                localFileStream = new FileInputStream(path);
+                File file = new File(path);
+
+                int len = (int) file.length();
+                tBuf = new byte[len];
+                int tBytesRead = localFileStream.read(tBuf);
+
+            }
+
+            try {
+                writer.writeFile(tBuf);
+            } catch (AlluxioException e) {
+                e.printStackTrace();
+            }
+
+            localFileStream.close();
+
+        }
+        is.close();
+
     }
 }
